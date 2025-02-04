35ef09ed3e82170bcc20eef4132385aa9e18370c
paulo <paulofvilarimda@gmail.com>
2025-02-04 11:39:03 -0300

    feat(home): implements home page

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,7 +1,8 @@
 export default function HomePage() {
   return (
-    <main className="pt-20">
-      <h1>Hello World</h1>
+    <main className="flex flex-col items-center gap-4 pt-20">
+      <h1 className="text-lg">Sistema de Distribuição de Arquivos</h1>
+      <h2>Segurança Computacional - T01</h2>
     </main>
   );
 }
