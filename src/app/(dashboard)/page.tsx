13ac2ea3306b3cebe780de7f665024aa20e8c3cb
paulo <paulofvilarimda@gmail.com>
2025-02-11 03:00:26 -0300

    fix home style

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,6 +1,6 @@
 export default function HomePage() {
   return (
-    <main className="flex h-screen flex-col items-center gap-4">
+    <main className="flex h-screen flex-col items-center justify-center gap-4">
       <h1 className="text-lg">Sistema de Distribuição de Arquivos</h1>
       <h2>Segurança Computacional - T01</h2>
     </main>
